7fa95933f2b64610867da67b6598e7ef9404deb2
Ricardo Fabio Caterino Polini <ricardocaterino@gmail.com>
2022-12-02 09:28:10 +0000

    App en blanco

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,21 +4,11 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>To Do App by Ricardo Caterino</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="text-center mt-5">
+      <h1>To Do App by Ricardo Caterino</h1>
+    </div>
+  );
 };
